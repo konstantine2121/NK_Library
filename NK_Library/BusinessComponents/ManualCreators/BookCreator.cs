858c746f580a4b66cb7ec4b51db5ac76e85bb528
Konstantine <kach91kach91@gmail.com>
2023-03-05 02:20:46 +0300

    Update BookCreator

--- NK_Library/BusinessComponents/ManualCreators/BookCreator.cs
+++ NK_Library/BusinessComponents/ManualCreators/BookCreator.cs
@@ -4,44 +4,77 @@ using NK_Library.Interfaces.BusinessComponents.ManualCreators;
 
 namespace NK_Library.BusinessComponents.ManualCreators
 {
+    /// <summary>
+    /// Регистрация книги из консоли.
+    /// </summary>
     internal class BookCreator : IManualCreator<Book>
     {
         public bool TryCreate(out Book item)
         {
+            item = null;
+
+            Output.PrintInfo("Регистрация новой книги.");
+            Output.PrintWarning($"Введите '{Enums.ConsoleComands.Exit}' для выхода.");
+
+            string author = default;
+            string bookName = default;
+            string genre = default;
+            bool adultOnly = default;
+            int yearOfPublication = default;
+
+            bool success = false;
+
+            success = InterruptableInput.TryReadString("Введите имя автора:", out author);
+
+            if (!success) 
+            {
+                PrintAbortWarning();
+                return false;
+            }
+
+            success = InterruptableInput.TryReadString("Введите имя книги:", out bookName);
+
+            if (!success)
+            {
+                PrintAbortWarning();
+                return false;
+            }
+            success = InterruptableInput.TryReadString("Введите жанр:", out genre);
+
+            if (!success)
+            {
+                PrintAbortWarning();
+                return false;
+            }
+            success = InterruptableInput.TryReadBoolean("Введите есть ли ограничение по возрасту (0 - нет / 1-есть):", out adultOnly);
+
+            if (!success)
+            {
+                PrintAbortWarning();
+                return false;
+            }
+            success = InterruptableInput.TryReadPositiveInteger("Введите год публикации:", out yearOfPublication);
+
+            if (!success)
+            {
+                PrintAbortWarning();
+                return false;
+            }
+
             item = new Book(
-                InputAuthor(),
-                InputBookName(),
-                InputGenre(),
-                InputIsAdultOnly(),
-                InputYearOfPublication()
+                author,
+                bookName,
+                genre,
+               adultOnly,
+                yearOfPublication
                 );
 
             return true;
         }
 
-        private string InputAuthor()
+        private void PrintAbortWarning()
         {
-            return Input.ReadString("Введите имя автора:");
-        }
-
-        private string InputBookName()
-        {
-            return Input.ReadString("Введите имя книги:");
-        }
-
-        private string InputGenre()
-        {
-            return Input.ReadString("Введите жанр:");
-        }
-
-        private bool InputIsAdultOnly()
-        {
-            return Input.ReadBoolean("Введите есть ли ограничение по возрасту:");
-        }
-
-        private int InputYearOfPublication()
-        {
-            return Input.ReadPositiveInteger("Введите год публикации:");
+            Output.PrintWarning("Ввод данных прерван.");
         }
     }
 }
